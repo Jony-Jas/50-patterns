3d867597665498ce9672791468a5add45b213f8c
Jony-Jas <jonyjas@gmail.com>
2022-01-07 15:07:42 +0530

    day 7

---
+++ pattern-13.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=n-1; j>=0; j--)
+        {
+            if (i==0 || j==i || j==n-1)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+            
+        }
+        cout<<endl;
+    }
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+for(int j=1; j<=n-i; j++)
+        {
+            cout<<" ";
+        }
+        for(int k=1;k<=i; k++)
+        {
+            cout<<"*";
+        }
