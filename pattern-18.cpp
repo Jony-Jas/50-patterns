7a81fcbfd5b4627421a436f5f7a7a6d7ff85756c
Jony-Jas <jonyjas@gmail.com>
2022-01-09 11:48:02 +0530

    day 9

--- pattern-18.cpp
+++ pattern-18.cpp
@@ -1,17 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
     int n;
-    cin>>n;
-    
-    for(int i=0; i < n; i++)
+    cin >> n;
+
+    for (int i = n; i > 0; i--)
     {
-        for(int j=1; j <= n-i; j++)
+        for (int j = 1; j <= i; j++)
         {
-            cout << j;
+            cout << i;
         }
-        cout<< endl;
+        cout << endl;
     }
 }
