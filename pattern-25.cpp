53ed27bd60871ebdbe918f919c1abf9739124727
Jony-Jas <jonyjas@gmail.com>
2022-01-13 10:55:33 +0530

    Day 13

---
+++ pattern-25.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j == (n / 2) && i == (n / 2))
+            {
+                cout << 0;
+                continue;
+            }
+            cout << 1;
+        }
+        cout << endl;
+    }
+}
